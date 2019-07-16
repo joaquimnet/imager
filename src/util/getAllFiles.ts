6733ff64f195b1311c9405a8c33df861906899cd
Popon <joaquimmy@yahoo.com>
2019-07-16 12:55:40 -0300

    ✨ Added support for .ts files

--- src/util/getAllFiles.ts
+++ src/util/getAllFiles.ts
@@ -1,5 +1,5 @@
-const path = require('path');
-const fs = require('fs');
+import fs from "fs";
+import path from "path";
 
 /**
  * Find all files inside a dir, recursively.
@@ -7,17 +7,16 @@ const fs = require('fs');
  * @param  {string} dir Dir path string.
  * @return {string[]} Array with all file names that are inside the directory.
  */
-const getAllFiles = dir =>
-  fs.readdirSync(dir).reduce((files, file) => {
+export function getAllFiles(dir: string): string[] {
+  return fs.readdirSync(dir).reduce<string[]>((files, file) => {
     const name = path.join(dir, file);
     const isDirectory = fs.statSync(name).isDirectory();
     if (isDirectory) {
       return [...files, ...getAllFiles(name)];
     } else {
-      return /^_/.test(file) || !/\.js$/.test(file)
+      return /^_/.test(file) || !/\.(js|ts)$/.test(file)
         ? [...files]
         : [...files, name];
     }
   }, []);
-
-module.exports = getAllFiles;
+}
